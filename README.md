128abd1d6ae8e87920eeb3506839d275d8d2bb7e
Sam Xie <samxie@outlook.com.au>
2019-05-24 14:15:31 +1000

    lesson 19

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Course Resource list:
 
 https://www.appbrewery.co/p/ios12-course-resources
+
+App Marketing Course Resource List:
+
+https://www.appbrewery.co/p/appmarketing-course-resources
---
+++ iamrich/hello_world/hello_world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello_world
+//
+//  Created by Sam Xie on 24/5/19.
+//  Copyright © 2019 Sam Xie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
